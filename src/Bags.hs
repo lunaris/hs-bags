49848fa8a6f5c0938b04a99e2df9627985d9856f
Will Jones <will@sacharissa.co.uk>
2017-08-17 20:46:41 +0100

    Hack up dynamic insertion.

--- src/Bags.hs
+++ src/Bags.hs
@@ -1,1 +1,43 @@
 module Bags where
+
+import qualified Data.Dynamic    as Dyn
+import qualified Data.Map.Strict as M
+import qualified Data.Text       as Tx
+import qualified GHC.Generics    as G
+
+newtype Bag field
+  = Bag { _bagMap :: M.Map Tx.Text Dyn.Dynamic }
+
+class Dyn.Typeable field => Field field where
+  keyOf :: field -> Tx.Text
+
+empty :: Bag field
+empty
+  = Bag M.empty
+
+insert :: Field field => field -> Bag field -> Bag field
+insert x (Bag bag)
+  = Bag (M.insert (keyOf x) (Dyn.toDyn x) bag)
+
+lookup :: (a -> field) -> Bag field -> Maybe a
+lookup
+  = undefined
+
+data ApplicationJourneyField
+  = AMortgageAmount MortgageAmount
+  | APropertyValue PropertyValue
+  deriving (G.Generic)
+
+instance Field ApplicationJourneyField where
+  keyOf
+    = \case
+        AMortgageAmount _ -> "MortgageAmount"
+        APropertyValue _  -> "PropertyValue"
+
+newtype MortgageAmount
+  = MortgageAmount { _mortgageAmountInt :: Int }
+  deriving (G.Generic)
+
+newtype PropertyValue
+  = PropertyValue { _propertyValueInt :: Int }
+  deriving (G.Generic)
