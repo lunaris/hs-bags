146b55512aeeffcf8f74040b2c3ce7be06684803
Will Jones <will@sacharissa.co.uk>
2017-08-18 08:55:12 +0100

    Something a bit more type-level.

--- src/Bags.hs
+++ src/Bags.hs
@@ -1,43 +1,94 @@
+{-# LANGUAGE AllowAmbiguousTypes        #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DeriveDataTypeable         #-}
+{-# LANGUAGE DeriveFoldable             #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DeriveTraversable          #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE FunctionalDependencies     #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE KindSignatures             #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE TupleSections              #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE UndecidableInstances       #-}
+
 module Bags where
 
-import qualified Data.Dynamic    as Dyn
+import           Data.Dynamic
 import qualified Data.Map.Strict as M
 import qualified Data.Text       as Tx
-import qualified GHC.Generics    as G
+import           GHC.Generics
+import           GHC.TypeLits
 
-newtype Bag field
-  = Bag { _bagMap :: M.Map Tx.Text Dyn.Dynamic }
+newtype Bag (fields :: [*])
+  = Bag { _bagMap :: M.Map Tx.Text Dynamic }
 
-class Dyn.Typeable field => Field field where
-  keyOf :: field -> Tx.Text
+data Field (name :: Symbol) (ty :: *)
 
-empty :: Bag field
+class (KnownSymbol name, Typeable ty)
+  =>  HasField (fields :: [*]) (name :: Symbol) (ty :: *) | fields name -> ty where
+
+instance (KnownSymbol name, Typeable ty, Lookup fields fields name ~ ty)
+      =>  HasField fields name ty
+
+type family Lookup (allFields :: [*]) (fields :: [*]) (name :: Symbol) :: * where
+  Lookup _ (Field name ty ': fields) name
+    = ty
+  Lookup allFields (field ': fields) name
+    = Lookup allFields fields name
+  Lookup '[] '[] name
+    = TypeError ('Text "There are no available fields for this type")
+  Lookup allFields '[] name
+    = TypeError
+        (     'Text "The field "
+        ':<>: 'ShowType name
+        ':<>: 'Text " could not be found. Please use one of:"
+        ':$$: FieldNamesError allFields
+        )
+
+type family FieldNamesError (fields :: [*]) :: ErrorMessage where
+  FieldNamesError '[Field name _]
+    =     'Text "  "
+    ':<>: 'ShowType name
+  FieldNamesError (Field name _ ': fields)
+    =     'Text "  "
+    ':<>: 'ShowType name
+    ':$$: FieldNamesError fields
+
+symbolText :: forall s. KnownSymbol s => Tx.Text
+symbolText
+  = Tx.pack (symbolVal (Proxy @s))
+
+empty :: Bag fields
 empty
   = Bag M.empty
 
-insert :: Field field => field -> Bag field -> Bag field
-insert x (Bag bag)
-  = Bag (M.insert (keyOf x) (Dyn.toDyn x) bag)
+insert :: forall name fields ty. HasField fields name ty => ty -> Bag fields -> Bag fields
+insert x (Bag m)
+  = Bag (M.insert (symbolText @name) (toDyn x) m)
 
-lookup :: (a -> field) -> Bag field -> Maybe a
-lookup
-  = undefined
+lookup :: forall name fields ty. HasField fields name ty => Bag fields -> Maybe ty
+lookup (Bag m)
+  = M.lookup (symbolText @name) m >>= fromDynamic
 
-data ApplicationJourneyField
-  = AMortgageAmount MortgageAmount
-  | APropertyValue PropertyValue
-  deriving (G.Generic)
-
-instance Field ApplicationJourneyField where
-  keyOf
-    = \case
-        AMortgageAmount _ -> "MortgageAmount"
-        APropertyValue _  -> "PropertyValue"
+type ApplicationJourneyFields
+  = '[ Field "MortgageAmount" MortgageAmount
+     , Field "PropertyValue"  PropertyValue
+     ]
 
 newtype MortgageAmount
   = MortgageAmount { _mortgageAmountInt :: Int }
-  deriving (G.Generic)
+  deriving (Generic, Show)
 
 newtype PropertyValue
   = PropertyValue { _propertyValueInt :: Int }
-  deriving (G.Generic)
+  deriving (Generic, Show)
